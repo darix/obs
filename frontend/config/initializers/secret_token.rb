d31c992adef5dde8735a9302316b15918b481caa
Marcus Rückert <darix@nordisch.org>
2011-02-23 23:03:06 +0100

    - added empty rails 3 app

---
+++ frontend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frontend::Application.config.secret_token = '64b61747cb417f4e0bfeb9e010ba24cdb6b3923629776c760ddd81aa20d2c196683470f61fc77fcf481debf59c007cd0a323e55d51d250a97a3a7839cc81a55c'
